2abd789665ffdba874750d345b5f2be30e43d4d4
Rodrigo Macedo <77795519+Rodrigomace@users.noreply.github.com>
2021-02-09 16:02:44 -0300

    Add files via upload
    Curso de Lógica de Progromação

---
+++ 49_desafio_atribuicao.js
@@ -0,0 +1,10 @@
+// concatenação de strings utilizando atribuição aditiva (+=)
+// \n -> quebra linha
+let texto = 'Lista de Aprovados\n'
+texto += '==================\n\n'
+texto += '1. Ana Silva\n'
+texto += '2. Pedro Albuquerque\n'
+texto += '3. Guilherme Pereira\n'
+texto += '4. Rebeca França'
+
+console.log(texto)
