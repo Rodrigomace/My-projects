29a212d1be515c9f3e8cb3445570625653874f0d
Rodrigo Macedo <77795519+Rodrigomace@users.noreply.github.com>
2021-02-19 21:11:12 -0300

    Add files via upload

---
+++ 98_desafio_for.js
@@ -0,0 +1,17 @@
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+
+// let linha = ''
+
+// for (let i = 1; i <= 6; i++) {
+//    linha += '#'
+//    console.log(linha)
+// }
+
+for (let s = '#'; s != '#######'; s += '#') { // laço for sem usar números
+    console.log(s)
+}
