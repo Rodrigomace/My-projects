cdac0234eb1978484a2d37b88c2b797087dbab7f
Rodrigo Macedo <77795519+Rodrigomace@users.noreply.github.com>
2021-02-24 15:48:28 -0300

    Add files via upload

---
+++ 109_desafio_funcao_1.js
@@ -0,0 +1,19 @@
+// função: 2 parâmetros -> console.log(+)
+function somar(n1, n2) {
+    console.log(n1 + n2)
+}
+
+//função: 2 parâmetros -> console.log(-)
+function subtrair(a, b) {
+    console.log(a - b)
+}
+
+// ...(3, 4)
+somar(3, 4)
+// ...(31, 98)
+somar(31, 98)
+
+// ...(11, 7)
+subtrair(11, 7)
+// ...(19, 52)
+subtrair(19, 52)
